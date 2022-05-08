4dfc4598a9798688a8e6aa0fa5a500bd81925790
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-08 07:31:25 +0600

    array.from , includes ,foreach

--- prblm-solving-p-1/app.js
+++ prblm-solving-p-1/app.js
@@ -3,8 +3,36 @@ function getRndNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
 //   console.log(getRndNum(1, 6))
-  
+// ---------------
 // amra amader class ar sokoler name alphabetically sajate pary?
 const names = ["bablu","ablu","zablu","pablu"]
 // remind : array.sort main array take change kore fele
-console.log(names.sort())
+// console.log(names.sort())
+//----------------
+// amra amder class ar sokoler name roll krome sajate pari?
+const rolls = [2,9,1,3,6,4,8,5]
+// console.log(rolls.sort((a,b)=>{return a-b}))
+//---------------
+// kono sal leap year ki na count kora jabe?
+const isLeapYear=(year)=>{
+    if(year % 400 ===0 || ((year%4===0)&&(year%100!==0))){
+        console.log(year, 'is leap year')
+    }else{
+        console.log(year,'is not leap year')
+    }
+}
+// isLeapYear(2028)
+//--------------
+// kono sentence a kotogula vowel ase taha nirnoy kora jabe?
+const vowels = ['a','e','i','o','u']
+const countVowel=(sentence)=>{
+    let count=0;
+    const letters = Array.from(sentence)
+       letters.forEach((value)=>{
+           if(vowels.includes(value)){
+               count++;
+           }
+       })
+       return count
+}
+console.log(countVowel('i love bangladesh'))
